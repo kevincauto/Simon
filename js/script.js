18c56c0fdbce93f022e3f901e45772fb827f0dc5
Kevin Cauto <kevincauto@aol.com>
2016-09-07 12:18:32 -0400

    added javaScript file in

---
+++ js/script.js
@@ -0,0 +1,28 @@
+alert("Hello World! 1");
+onOff="off";
+arr = [];
+strictMode = "off";
+turnNum = "0"
+//turn on machine
+function onIt(){
+  onOff = "on";
+  //panels light up and make a sound
+  alert("Hello World!");
+  //display becomes enabled.
+}
+//when you hit the start button
+function start(){
+//generate random arr of 20 values r b y g
+
+//display starts with the number 1
+}
+function strict(){
+  if(strictMode == "on"){
+    strictMode = "off";
+  }
+  else{strictMode = "on";}
+}
+fucntion off(){
+  onOff="off";
+  //reset
+}
